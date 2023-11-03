56badfec51c819ca53cc196f8b6d32229d25c97c
Felipe Dias <felipedias.dev@gmail.com>
2023-11-03 02:06:17 -0300

    feat: :sparkles: update handler and routes to use new file organization

--- handler/createOpening.go
+++ handler/createOpening.go
@@ -3,11 +3,35 @@ package handler
 import (
 	"net/http"
 
+	"github.com/felipedias-dev/gopportunities/schema"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "POST Opening",
-	})
+	request := CreateOpeningReq{}
+
+	if err := ctx.ShouldBindJSON(&request); err != nil {
+		logger.Errorf("validation error: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	logger.Infof("request received: %+v", request)
+
+	opening := schema.Opening{
+		Role:     request.Role,
+		Company:  request.Company,
+		Location: request.Location,
+		Remote:   request.Remote,
+		Salary:   request.Salary,
+		Link:     request.Link,
+	}
+
+	if err := db.Create(&opening).Error; err != nil {
+		logger.Errorf("creating opening error: %v", err.Error())
+		sendError(ctx, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	sendSuccess(ctx, http.StatusCreated, opening)
 }
